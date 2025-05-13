cce62d05e8182d0e4088657eea1063c11df85bd6
koreannn <ghdtjdwo5@gamil.com>
2025-05-13 12:04:47 +0900

    이진탐색트리 데이터 삭제 구현

--- 5.BinarySearchTree/binary_search_tree.py
+++ 5.BinarySearchTree/binary_search_tree.py
@@ -39,9 +39,9 @@ class BinaryTree:
     def inOrderTraversal(self, tree):
         if tree == None:
             return
-        self.inOrderTraversal(tree.getLeftSubTree())    # 왼쪽 서브트리 순회
-        print(tree.data)                 # 현재 노드 데이터 출력
-        self.inOrderTraversal(tree.getRightSubTree())   # 오른쪽 서브트리 순회
+        self.inOrderTraversal(tree.getLeftSubTree())
+        print(tree.data)
+        self.inOrderTraversal(tree.getRightSubTree())
         
     def postOrderTraversal(self, tree):
         if tree == None:
@@ -51,6 +51,17 @@ class BinaryTree:
         self.postOrderTraversal(tree.getRightSubTree())
         print(tree.data)
         
+    def removeLeftSubTree(self):
+        removed_tree = self.getLeftSubTree()
+        self.setLeftSubTree(None)
+        return removed_tree
+        
+    def removeRightSubTree(self):
+        removed_tree = self.getRightSubTree()
+        self.setRightSubTree(None)
+        return removed_tree
+    
+
 """
 이진 탐색 트리 구현
 """
@@ -80,7 +91,7 @@ class BinarySearchTree:
         # 깂(서브트리) 삽입
         new_node = BinaryTree(data)
         
-        if parent_node.getData() > data:
+        if parent_node.getData() > data: # (parrent_node == curr_node)
             parent_node.setLeftSubTree(new_node)
         else:
             parent_node.setRightSubTree(new_node)
@@ -97,6 +108,80 @@ class BinarySearchTree:
                 curr_node = curr_node.getRightSubTree()
         
         return None
+    
+    
+    def remove(self, target):
+        fakeParentRootNode = BinaryTree(0)  # 루트 노드가 제거되는 경우 필요
+        parentNode = fakeParentRootNode
+        curr_node = self.root
+        deleted_node = None
+        
+        fakeParentRootNode.setRightSubTree(self.root)  # setLeftSubTree()로 해도 상관없음
+        
+        # 삭제할 노드 찾기
+        while curr_node is not None and curr_node.getData() != target:
+            parentNode = curr_node
+            
+            if curr_node.getData() > target:
+                curr_node = curr_node.getLeftSubTree()
+            else:
+                curr_node = curr_node.getRightSubTree()
+        
+        # 삭제할 노드를 찾지 못한 경우
+        if curr_node is None:
+            return None
+            
+        # Case 분류
+        deleted_node = curr_node
+        
+        # 1. 제거하는 노드가 터미널 노드인 경우
+        if (deleted_node.getLeftSubTree() is None and deleted_node.getRightSubTree() is None):
+            if parentNode.getLeftSubTree() == deleted_node:
+                parentNode.removeLeftSubTree()
+            else:
+                parentNode.removeRightSubTree()
+                
+        # 2. 제거하는 노드의 자식 노드가 한 개인 경우
+        elif deleted_node.getLeftSubTree() is None or deleted_node.getRightSubTree() is None:
+            deleted_node_child = None
+            # 제거할 노드의 자식 노드 찾기
+            if deleted_node.getLeftSubTree() is not None:
+                deleted_node_child = deleted_node.getLeftSubTree()
+            else:
+                deleted_node_child = deleted_node.getRightSubTree()
+                
+            # 제거 후 자식 트리로 대신 연결
+            if parentNode.getLeftSubTree() == deleted_node:
+                parentNode.setLeftSubTree(deleted_node_child)
+            else:
+                parentNode.setRightSubTree(deleted_node_child)
+                
+        # 3. 제거하는 노드의 자식 노드가 두 개인 경우
+        else:
+            replacingNode = deleted_node.getLeftSubTree()
+            replacingNodeParent = deleted_node
+            
+            # 왼쪽 서브트리에서 가장 큰 값 찾기
+            while replacingNode.getRightSubTree() is not None:
+                replacingNodeParent = replacingNode
+                replacingNode = replacingNode.getRightSubTree()
+            
+            # 데이터 교환
+            deleted_node.setData(replacingNode.getData())
+            
+            # replacingNode 제거
+            if replacingNodeParent.getLeftSubTree() == replacingNode:
+                replacingNodeParent.setLeftSubTree(replacingNode.getLeftSubTree())
+            else:
+                replacingNodeParent.setRightSubTree(replacingNode.getLeftSubTree())
+            
+            deleted_node = replacingNode
+        
+        # 루트 노드가 변경된 경우 처리
+        if fakeParentRootNode.getRightSubTree() != self.root:
+            self.root = fakeParentRootNode.getRightSubTree()
+        
+        return deleted_node
 
 
 bst = BinarySearchTree()
@@ -117,9 +202,14 @@ bst.insert(35)
 bst.insert(37)
 bst.root.inOrderTraversal(bst.root) # 중위 순회
 
-print("========== Search 6 ==========");
-print(bst.search(6));
+print("========== Search 6 ==========")
+print(bst.search(6))
 
 
-print("========== Search 1 ==========");
-print(bst.search(1));
+print("========== Search 1 ==========")
+print(bst.search(1))
+
+# remove메서드 테스트
+print("========== Remove Test ==========")
+bst.remove(10)
+bst.root.inOrderTraversal(bst.root)
