7756ee682cad18ae85b7e7f9810197936f1eaf4d
koreannn <ghdtjdwo5@gamil.com>
2025-05-15 12:20:09 +0900

    BinaryTree분리

--- 5.BinarySearchTree/binary_search_tree.py
+++ 5.BinarySearchTree/binary_search_tree.py
@@ -1,66 +1,7 @@
+from binary_tree import BinaryTree
 """
 ADT: 데이터의 삽입, 삭제, 제거
 """
-
-"""
-이진트리 객체 가져오기(이전 코드와 동일)
-"""
-class BinaryTree:
-    def __init__(self, data, LeftTree=None, RightTree=None):
-        self.data = data
-        self.LeftTree = LeftTree
-        self.RightTree = RightTree
-    
-    def getData(self):
-        return self.data
-
-    def setData(self, data):
-        self.data = data
-        
-    def getLeftSubTree(self):
-        return self.LeftTree
-
-    def getRightSubTree(self):
-        return self.RightTree
-    
-    def setLeftSubTree(self, tree):
-        self.LeftTree = tree
-    
-    def setRightSubTree(self, tree):
-        self.RightTree = tree
-    
-    def preOrderTraversal(self, tree):
-        if tree == None:
-            return
-        print(tree.data)
-        self.preOrderTraversal(tree.getLeftSubTree())
-        self.preOrderTraversal(tree.getRightSubTree())
-        
-    def inOrderTraversal(self, tree):
-        if tree == None:
-            return
-        self.inOrderTraversal(tree.getLeftSubTree())
-        print(tree.data)
-        self.inOrderTraversal(tree.getRightSubTree())
-        
-    def postOrderTraversal(self, tree):
-        if tree == None:
-            return
-
-        self.postOrderTraversal(tree.getLeftSubTree())
-        self.postOrderTraversal(tree.getRightSubTree())
-        print(tree.data)
-        
-    def removeLeftSubTree(self):
-        removed_tree = self.getLeftSubTree()
-        self.setLeftSubTree(None)
-        return removed_tree
-        
-    def removeRightSubTree(self):
-        removed_tree = self.getRightSubTree()
-        self.setRightSubTree(None)
-        return removed_tree
-    
 
 """
 이진 탐색 트리 구현
---
+++ 5.BinarySearchTree/binary_tree.py
@@ -0,0 +1,57 @@
+"""
+이진트리 객체 가져오기(이전 코드와 동일)
+"""
+class BinaryTree:
+    def __init__(self, data, LeftTree=None, RightTree=None):
+        self.data = data
+        self.LeftTree = LeftTree
+        self.RightTree = RightTree
+    
+    def getData(self):
+        return self.data
+
+    def setData(self, data):
+        self.data = data
+        
+    def getLeftSubTree(self):
+        return self.LeftTree
+
+    def getRightSubTree(self):
+        return self.RightTree
+    
+    def setLeftSubTree(self, tree):
+        self.LeftTree = tree
+    
+    def setRightSubTree(self, tree):
+        self.RightTree = tree
+    
+    def preOrderTraversal(self, tree):
+        if tree is None:
+            return
+        print(tree.data)
+        self.preOrderTraversal(tree.getLeftSubTree())
+        self.preOrderTraversal(tree.getRightSubTree())
+        
+    def inOrderTraversal(self, tree):
+        if tree is None:
+            return
+        self.inOrderTraversal(tree.getLeftSubTree())
+        print(tree.data)
+        self.inOrderTraversal(tree.getRightSubTree())
+        
+    def postOrderTraversal(self, tree):
+        if tree is None:
+            return
+        self.postOrderTraversal(tree.getLeftSubTree())
+        self.postOrderTraversal(tree.getRightSubTree())
+        print(tree.data)
+        
+    def removeLeftSubTree(self):
+        removed_tree = self.getLeftSubTree()
+        self.setLeftSubTree(None)
+        return removed_tree
+        
+    def removeRightSubTree(self):
+        removed_tree = self.getRightSubTree()
+        self.setRightSubTree(None)
+        return removed_tree
